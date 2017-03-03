b098ffb363474808470bcf0e5b7fc0678d676f10
Ryan <rab83@duke.edu>
2017-03-03 16:51:55 -0500

    color scale for scatter plot can now be continuout or discrete

--- ui.R
+++ ui.R
@@ -1,16 +1,6 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-#datafile <- read.csv("final_motor_merged.csv", fileEncoding = "iso-8859-1")
-#datafile <- read.csv("final_motor_merged_cleaned.csv", fileEncoding = "iso-8859-1")
-datafile <- read.csv("final_cdi_merged_cleaned.csv", fileEncoding = "iso-8859-1")
+
+datafile <- read_csv("final_cdi_merged_cleaned.csv")
 Vchoices <- 1:ncol(datafile)
 names(Vchoices) <- names(datafile)
 
@@ -28,9 +18,9 @@ shinyUI(fluidPage(
                   choices = Vchoices,
                   multiple = TRUE),
       br(),
-      helpText("Select the columns you wish to view in the scatter plot.
+      helpText("Select the two variables you wish to view in the scatter plot.
                First choice = X-axis; Second choice = Y-axis."),
-      selectizeInput("scatChoices", label = "Select Columns for Scatter Plot: ",
+      selectizeInput("scatChoices", label = "Select Variables for Scatter Plot: ",
                   choices = Vchoices, selected = 5,
                   options = list(maxItems = 2)),
       br(),
