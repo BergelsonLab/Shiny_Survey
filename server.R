244072c3fc6473272f746d16b76ae0e5c37f57d4
Ryan <rab83@duke.edu>
2017-03-30 12:51:47 -0400

    understand, talk, gestures work with the application

--- server.R
+++ server.R
@@ -3,145 +3,204 @@ library(DT)
 library(tidyverse)
 library(ggplot2)
 
+datafile1 <- read.csv("final_cdi_merged_cleaned.csv")
+
+for(n in names(datafile1[,c(17:19, 458:508)])){
+  datafile1[[n]] <- ifelse(datafile1[[n]] == 'Yes',1,0)
+}
+
+dfile <- read.csv("final_cdi_merged_cleaned.csv")
+
+dfile1 <- mutate(dfile,
+                 Fraction_Total_Understand = rowSums(datafile1[,c(17:47, 50:445)] == 1)/425,
+                 Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1)/395,
+                 Fraction_Total_Gesture = rowSums(datafile1[,458:508])/51)
 
 shinyServer(function(input, output) {
-  datafile1 <- read.csv("final_cdi_merged_cleaned.csv")
   datafile <- mutate(datafile1,
-                     #Total_Understand = rowSums(datafile1[,20:47]),
                      Fraction_Total_Understand = rowSums(datafile1[,c(17:47, 50:445)] == 1)/425,
-                     Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1)/395)
-  observeEvent(input$colChoices, {
-    cols <- as.numeric(input$colChoices)
-    if(length(input$colChoices) == 1){
-      df <- data.frame(datafile[,cols])
-      names(df) <- names(datafile)[cols]
-      output$table = DT::renderDataTable(df, filter = "top")
-    }
-    else{
-      output$table = DT::renderDataTable(datafile[,cols], filter = "top")
-    }
-  })
+                     Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1)/395,
+                     Fraction_Total_Gesture = rowSums(datafile1[,458:508])/51)
   
-  datafile1 <- read.csv("final_cdi_merged_cleaned.csv")
-  datafile2 <- mutate(datafile1,
-                     #Total_Understand = rowSums(datafile1[,20:47]),
-                     Fraction_Total_Understand = rowSums(datafile1[,c(17:47, 50:445)] == 1)/425,
-                     Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1)/395)
-  observeEvent(input$scatChoices, {
-    colorVar <- as.numeric(input$color_var)
-    cols2 <- as.numeric(input$scatChoices)
-    if(length(input$scatChoices) == 1){
-      df2 <- data.frame(datafile2[,cols2])
-      names(df2) <- names(datafile2)[cols2]
-    }
-    
-    else if(cols2[2] > 510){
-      output$plot = renderPlot({
-        x_lab <- colnames(datafile2)[cols2[1]]
-        y_lab <- colnames(datafile2)[cols2[2]]
+  x <- reactive({input$radioButton})
+
+  output$plot<-renderPlot({
+    if(x() == 1){
+      datafile_understand <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI, 
+                                    Understand_childname:Understand_goforride, 
+                                    Fraction_Total_Understand)
+      datafile_understand_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
+                                          Child_gender, Understand_childname:Understand_goforride,
+                                          Fraction_Total_Understand)
+      colorVar <- as.numeric(input$color_var_understand)
+      cols2 <- as.numeric(input$plotChoices_Understand)
+      cols <- as.numeric(input$colChoices_understand)
+
+      if(length(input$colChoices_understand) == 1){
+        df <- data.frame(datafile_understand_table[,cols])
+        names(df) <- names(datafile_understand_table)[cols]
+        output$table <- DT::renderDataTable(df, filter = "top")
+      }
+      else{
+        output$table <- DT::renderDataTable(datafile_understand_table[,cols], filter = "top")
+      }
+      
+      if(length(input$plotChoices_Understand) == 1){
+        df2 <- data.frame(datafile_understand[,cols2])
+        names(df2) <- names(datafile_understand)[cols2]
+      }
+      
+      else if(cols2[2] > 33){
+        x_lab <- colnames(datafile_understand)[cols2[1]]
+        y_lab <- colnames(datafile_understand)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
-        colorV <- colnames(datafile2)[colorVar]
+        colorV <- colnames(dfile1)[colorVar]
         
-        p <- ggplot(data = datafile2) +
-          geom_point(mapping = aes(x = datafile2[cols2[1]],
-                                   y = datafile2[cols2[2]],
-                                   color = datafile2[[colorV]]),
+        p <- ggplot(data = datafile_understand) +
+          geom_point(mapping = aes(x = datafile_understand[cols2[1]],
+                                   y = datafile_understand[cols2[2]],
+                                   color = dfile1[[colorV]]),
                      position = position_jitter(w = 0.2, h = 0.1),
                      alpha = .6) +
-          stat_smooth(mapping = aes(x = datafile2[cols2[1]],
-                                    y = datafile2[cols2[2]]))+
+          stat_smooth(mapping = aes(x = datafile_understand[cols2[1]],
+                                    y = datafile_understand[cols2[2]]))+
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
         
         p + labs(x = x_lab , y = y_lab, color = colorV)
-      })
-    }
-    
-    else{
-      output$plot = renderPlot({
-        x_lab <- colnames(datafile2)[cols2[1]]
-        y_lab <- colnames(datafile2)[cols2[2]]
+      }
+      else{
+        x_lab <- colnames(datafile_understand)[cols2[1]]
+        y_lab <- colnames(datafile_understand)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
-        colorV <- colnames(datafile2)[colorVar]
+        colorV <- colnames(dfile1)[colorVar]
         
-        p <- ggplot(data = datafile2) +
-          geom_bar(mapping = aes(x = datafile2[cols2[1]],
-                                 y = datafile2[cols2[2]],
-                                 color = datafile2[[colorV]]),
-                                 stat = "identity")
+        p <- ggplot(data = datafile_understand) +
+          geom_bar(mapping = aes(x = datafile_understand[cols2[1]],
+                                 y = datafile_understand[cols2[2]],
+                                 color = dfile1[[colorV]]),
+                   stat = "identity")+
+          ggtitle(title)+
+          theme(plot.title = element_text(hjust = 0.5))
         p + labs(x = x_lab, y = y_lab, color = colorV)
-      })
+      }
     }
-  })
-    
-  # observeEvent(input$color_var, {
-  #   colorVar <- as.numeric(input$color_var)
-  #   cols2 <- as.numeric(input$scatChoices)
-  #   if(length(input$scatChoices) != 1){
-  #     output$plot = renderPlot({
-  #       x_lab <- colnames(datafile2)[cols2[1]]
-  #       y_lab <- colnames(datafile2)[cols2[2]]
-  #       title <- paste(x_lab, "vs.", y_lab, sep = " ")
-  #       colorV <- colnames(datafile2)[colorVar]
-  #       
-  #       p <- ggplot(data = datafile2) +
-  #         geom_point(mapping = aes(x = datafile2[cols2[1]],
-  #                                  y = datafile2[cols2[2]],
-  #                                  color = datafile2[[colorV]]),
-  #                    position = position_jitter(w = 0.2, h = 0.1),
-  #                    alpha = .6) +
-  #         stat_smooth(mapping = aes(x = datafile2[cols2[1]],
-  #                                   y = datafile2[cols2[2]]))+
-  #         ggtitle(title)+
-  #         theme(plot.title = element_text(hjust = 0.5))
-  #       
-  #       p + labs(x = x_lab , y = y_lab, color = colorV)
-  #     })
-  #   }
-  # })
-  
-  observeEvent(input$color_var, {
-    colorVar <- as.numeric(input$color_var)
-    cols2 <- as.numeric(input$scatChoices)
-    if(length(input$scatChoices) == 1){
-      df2 <- data.frame(datafile2[,cols2])
-      names(df2) <- names(datafile2)[cols2]
-    }
-    else if(cols2[2] > 510){
-      output$plot = renderPlot({
-        x_lab <- colnames(datafile2)[cols2[1]]
-        y_lab <- colnames(datafile2)[cols2[2]]
+    else if(x() == 2){
+      datafile_talk <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
+                              Talk_baabaa:Talk_some, Fraction_Total_Talk)
+      datafile_talk_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
+                                    Child_gender, Talk_baabaa:Talk_some, Fraction_Total_Talk)
+      
+      colorVar <- as.numeric(input$color_var_talk)
+      cols2 <- as.numeric(input$plotChoices_Talk)
+      cols <- as.numeric(input$colChoices_talk)
+      
+      if(length(input$colChoices_talk) == 1){
+        df <- data.frame(datafile_talk_table[,cols])
+        names(df) <- names(datafile_talk_table)[cols]
+        output$table = DT::renderDataTable(df, filter = "top")
+      }
+      else{
+        output$table = DT::renderDataTable(datafile_talk_table[,cols], filter = "top")
+      }
+      
+      if(length(input$plotChoices_Talk) == 1){
+        df2 <- data.frame(datafile_talk[,cols2])
+        names(df2) <- names(datafile_talk)[cols2]
+      }
+      
+      else if(cols2[2] > 398){
+        x_lab <- colnames(datafile_talk)[cols2[1]]
+        y_lab <- colnames(datafile_talk)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
-        colorV <- colnames(datafile2)[colorVar]
+        colorV <- colnames(dfile1)[colorVar]
         
-        p <- ggplot(data = datafile2) +
-          geom_point(mapping = aes(x = datafile2[cols2[1]],
-                                   y = datafile2[cols2[2]],
-                                   color = datafile2[[colorV]]),
+        p <- ggplot(data = datafile_talk) +
+          geom_point(mapping = aes(x = datafile_talk[cols2[1]],
+                                   y = datafile_talk[cols2[2]],
+                                   color = dfile1[[colorV]]),
                      position = position_jitter(w = 0.2, h = 0.1),
                      alpha = .6) +
-          stat_smooth(mapping = aes(x = datafile2[cols2[1]],
-                                    y = datafile2[cols2[2]]))+
+          stat_smooth(mapping = aes(x = datafile_talk[cols2[1]],
+                                    y = datafile_talk[cols2[2]]))+
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
         
         p + labs(x = x_lab , y = y_lab, color = colorV)
-      })
-    }
-    else{
-      output$plot = renderPlot({
-        x_lab <- colnames(datafile2)[cols2[1]]
-        y_lab <- colnames(datafile2)[cols2[2]]
+      }
+      else{
+        x_lab <- colnames(datafile_talk)[cols2[1]]
+        y_lab <- colnames(datafile_talk)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
-        colorV <- colnames(datafile2)[colorVar]
+        colorV <- colnames(dfile1)[colorVar]
         
-        p <- ggplot(data = datafile2) +
-          geom_bar(mapping = aes(x = datafile2[cols2[1]],
-                                 y = datafile2[cols2[2]],
-                                 color = datafile2[[colorV]]),
-                   stat = "identity")
+        p <- ggplot(data = datafile_talk) +
+          geom_bar(mapping = aes(x = datafile_talk[cols2[1]],
+                                 y = datafile_talk[cols2[2]],
+                                 color = dfile1[[colorV]]),
+                   stat = "identity")+
+          ggtitle(title)+
+          theme(plot.title = element_text(hjust = 0.5))
         p + labs(x = x_lab, y = y_lab, color = colorV)
-      })
+      }
+    }
+    else if(x() == 3){
+      datafile_gesture <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
+                                 Gestures_peekaboo:Gestures_wearglasses, Fraction_Total_Gesture)
+      datafile_gesture_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
+                                       Child_gender, Gestures_peekaboo:Gestures_wearglasses,
+                                       Fraction_Total_Gesture)
+
+      cols <- as.numeric(input$colChoices_gesture)
+      if(length(input$colChoices_gesture) == 1){
+        df <- data.frame(datafile_gesture_table[,cols])
+        names(df) <- names(datafile_gesture_table)[cols]
+        output$table = DT::renderDataTable(df, filter = "top")
+      }
+      else{
+        output$table = DT::renderDataTable(datafile_gesture_table[,cols], filter = "top")
+      }
+
+      colorVar <- as.numeric(input$color_var_gestures)
+      cols2 <- as.numeric(input$plotChoices)
+      if(length(input$plotChoices) == 1){
+        df2 <- data.frame(datafile_gesture[,cols2])
+        names(df2) <- names(datafile_gesture)[cols2]
+      }
+      else if(cols2[2] > 53){
+        x_lab <- colnames(datafile_gesture)[cols2[1]]
+        y_lab <- colnames(datafile_gesture)[cols2[2]]
+        title <- paste(x_lab, "vs.", y_lab, sep = " ")
+        colorV <- colnames(dfile1)[colorVar]
+
+        p <- ggplot(data = datafile_gesture) +
+          geom_point(mapping = aes(x = datafile_gesture[cols2[1]],
+                                   y = datafile_gesture[cols2[2]],
+                                   color = dfile1[[colorV]]),
+                     position = position_jitter(w = 0.2, h = 0.1),
+                     alpha = .6) +
+          stat_smooth(mapping = aes(x = datafile_gesture[cols2[1]],
+                                    y = datafile_gesture[cols2[2]]))+
+          ggtitle(title)+
+          theme(plot.title = element_text(hjust = 0.5))
+
+        p + labs(x = x_lab , y = y_lab, color = colorV)
+      }
+      else{
+        x_lab <- colnames(datafile_gesture)[cols2[1]]
+        y_lab <- colnames(datafile_gesture)[cols2[2]]
+        title <- paste(x_lab, "vs.", y_lab, sep = " ")
+        colorV <- colnames(dfile1)[colorVar]
+
+        p <- ggplot(data = datafile_gesture) +
+          geom_bar(mapping = aes(x = datafile_gesture[cols2[1]],
+                                 y = datafile_gesture[cols2[2]],
+                                 color = dfile1[[colorV]]),
+                   stat = "identity") +
+          ggtitle(title)+
+          theme(plot.title = element_text(hjust = 0.5))
+        p + labs(x = x_lab, y = y_lab, color = colorV)
+      }
     }
   })
-  })
+})
--- ui.R
+++ ui.R
@@ -6,26 +6,55 @@ library(ggplot2)
 
 datafile1 <- read.csv("final_cdi_merged_cleaned.csv")
 
+for(n in names(datafile1[,c(17:19, 458:508)])){
+  datafile1[[n]] <- ifelse(datafile1[[n]] == 'Yes',1,0)
+}
+
 datafile <- mutate(datafile1,
-                   Fraction_Total_Understand = rowSums(datafile1[,c(17:47, 50:445)] > 0 & 
-                                                datafile1[,c(17:47, 50:445)] < 2),
-                   Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1))
+                   Fraction_Total_Understand = rowSums(datafile1[,c(17:47, 50:445)] == 1)/425,
+                   Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1)/395,
+                   Fraction_Total_Gesture = rowSums(datafile1[,458:508])/51)
+
+table_choices <- 1:ncol(datafile)
+names(table_choices) <- names(datafile)
 
 
-Vchoices <- 1:ncol(datafile)
-names(Vchoices) <- names(datafile)
+datafile_understand_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
+                                    Child_gender, Understand_childname:Understand_goforride,
+                                    Fraction_Total_Understand)
+Vchoices_table_understand <- 1:ncol(datafile_understand_table)
+names(Vchoices_table_understand) <- gsub("Understand_","",names(datafile_understand_table))
 
-sumChoices <- select(datafile, Understand_childname:Talk_some)
 
-#mutate(datafile,from columns 17-445
-#       Understand_Sum = ##Sum add if column name starts with "Understand"
-                        ## and value = "Sometimes" or "Often" or 1 or "Yes"
-                        ## or if column name starts with "Talk" and value 
-                        ## = 1,
-#       Talk_Sum = ##Sum add if column name starts with "Talk" and value 
-                  ## = 2,
-#       Gesture_Sum = ##Sum add if column name starts with "Gestures" and
-                      ##Value = "Yes")
+datafile_talk_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
+                              Child_gender, Talk_baabaa:Talk_some, Fraction_Total_Talk)
+Vchoices_table_talk <- 1:ncol(datafile_talk_table)
+names(Vchoices_table_talk) <- gsub("Talk_","",names(datafile_talk_table))
+
+
+datafile_gesture_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
+                                Child_gender, Gestures_peekaboo:Gestures_wearglasses, Fraction_Total_Gesture)
+Vchoices_table_gesture <- 1:ncol(datafile_gesture_table)
+names(Vchoices_table_gesture) <- gsub("Gestures_","",names(datafile_gesture_table))
+
+
+datafile_understand <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI, 
+                              Understand_childname:Understand_goforride, 
+                              Fraction_Total_Understand)
+Vchoices_understand <- 1:ncol(datafile_understand)
+names(Vchoices_understand) <- gsub("Understand_","",names(datafile_understand))
+
+
+datafile_talk <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
+                        Talk_baabaa:Talk_some, Fraction_Total_Talk)
+Vchoices_talk <- 1:ncol(datafile_talk)
+names(Vchoices_talk) <- gsub("Talk_","",names(datafile_talk))
+
+
+datafile_gesture <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
+                           Gestures_peekaboo:Gestures_wearglasses, Fraction_Total_Gesture)
+Vchoices_gesture <- 1:ncol(datafile_gesture)
+names(Vchoices_gesture) <- gsub("Gestures_","",names(datafile_gesture))
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -36,22 +65,72 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      helpText("Select the columns that you wish to view in the data table."),
-      selectInput("colChoices", label = "Select Columns for Data Table: ",
-                  choices = Vchoices,
-                  multiple = TRUE),
+      helpText("Select word/phrase options to analyze:"),
+      radioButtons("radioButton", label = "Select set of words/phrases: ",
+                         choices = list("Understand" = 1, "Talk" = 2, "Gestures" = 3)),
       br(),
-      helpText("Select the two variables you wish to view in the scatter plot.
+      conditionalPanel(
+        condition = "input.radioButton == 1",
+        helpText("Select the columns that you wish to view in the data table
+                 (0 = child does not understand the word, 1 = child understands the word)."),
+        selectInput("colChoices_understand", label = "Select Columns for Data Table: ",
+                    choices = Vchoices_table_understand,
+                    multiple = TRUE)),
+      
+      conditionalPanel(
+        condition = "input.radioButton == 2",
+        helpText("Select the columns that you wish to view in the data table 
+                 (0 = child can not say the word, 1 = child can say the word)."),
+        selectInput("colChoices_talk", label = "Select Columns for Data Table: ",
+                    choices = Vchoices_table_talk,
+                    multiple = TRUE)),
+
+      conditionalPanel(
+        condition = "input.radioButton == 3",
+        helpText("Select the columns that you wish to view in the data table 
+                 (0 = child does not make the gesture, 1 = child makes the gesture)."),
+        selectInput("colChoices_gesture", label = "Select Columns for Data Table: ",
+                    choices = Vchoices_table_gesture,
+                    multiple = TRUE)),
+      br(),
+      conditionalPanel(
+        condition = "input.radioButton == 1",
+        helpText("Select the two variables you wish to view in the plot.
                First choice = X-axis; Second choice = Y-axis."),
-      selectizeInput("scatChoices", label = "Select Variables for Scatter Plot: ",
-                  choices = Vchoices, selected = 5,
-                  options = list(maxItems = 2)),
-      br(),
-      helpText("Select the variable with respect to which the scatter plot colors
+        selectizeInput("plotChoices_Understand", label = "Understand Word Choices: ",
+                       choices = Vchoices_understand, selected = 1,
+                       options = list(maxItems = 2)),
+        helpText("Select the variable with respect to which the plot colors
                will be filtered."),
-      selectInput("color_var", label = "Select Variable for Plot Color Scale:",
-                  choices = Vchoices, selected = 4,
-                  multiple = FALSE)
+        selectInput("color_var_understand", label = "Select Variable for Understand Plot Color Scale:",
+                    choices = table_choices, selected = 4,
+                    multiple = FALSE)),
+      
+      conditionalPanel(
+        condition = "input.radioButton == 2",
+        helpText("Select the two variables you wish to view in the plot.
+               First choice = X-axis; Second choice = Y-axis."),
+        selectizeInput("plotChoices_Talk", label = "Spoken Word Choices: ",
+                       choices = Vchoices_talk, selected = 1,
+                       options = list(maxItems = 2)),
+        helpText("Select the variable with respect to which the plot colors
+               will be filtered."),
+        selectInput("color_var_talk", label = "Select Variable for Talk Plot Color Scale:",
+                    choices = table_choices, selected = 4,
+                    multiple = FALSE)),
+      
+      conditionalPanel(
+        condition = "input.radioButton == 3",
+        helpText("Select the two variables you wish to view in the plot.
+                First choice = X-axis; Second choice = Y-axis."),
+        selectizeInput("plotChoices", label = "Select Gestures: ",
+                       choices = Vchoices_gesture, selected = 1,
+                       options = list(maxItems = 2)),
+        helpText("Select the variable with respect to which the plot colors
+                 will be filtered."),
+        selectInput("color_var_gestures", label = "Select Variable for Gestures Plot Color Scale:",
+                    choices = table_choices, selected = 4,
+                    multiple = FALSE))
     ),
   
     # Show a plot of the generated distribution
