504ca5b824f14119ff26354015bc65ea6e8cc397
Ryan <rab83@duke.edu>
2017-04-07 12:47:52 -0400

    Allows users to download csv files of data; filtered and not filtered. Contains comments throughout the code as well.

--- server.R
+++ server.R
@@ -3,22 +3,38 @@ library(DT)
 library(tidyverse)
 library(ggplot2)
 
+# Read in csv file with survey data
 datafile1 <- read.csv("final_cdi_merged_cleaned.csv")
 
+# Convert "Yes" values to 1; "No" values to 0 in csv file
 for(n in names(datafile1[,c(17:19, 458:508)])){
   datafile1[[n]] <- ifelse(datafile1[[n]] == 'Yes',1,0)
 }
 
+# Displays outputs based on inputs from user
 shinyServer(function(input, output) {
+  # Create new columns for datafile; fraction_total_Understand/Talk/Gesture
   datafile <- mutate(datafile1,
                      Fraction_Total_Understand = rowSums(datafile1[,c(17:47, 50:445)] == 1)/425,
                      Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1)/395,
                      Fraction_Total_Gesture = rowSums(datafile1[,458:508])/51)
   
+  # Allow user to download data in csv file before filtering
+  output$downloadData <- downloadHandler(
+    filename = "Seedling_Survey_AllData.csv",
+    content = function(file_out){
+      write.csv(datafile,file_out)
+    }
+  )
+  
+  #Reactive element based on user input on radioButton to alter which set of data is being analyzed
   x <- reactive({input$radioButton})
 
+
   output$plot<-renderPlot({
+    #If user selects "Understand" in radioButton
     if(x() == 1){
+      #Create new dataframes for plot and table with columns representing "understand" data only
       datafile_understand <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI, 
                                     Understand_childname:Understand_goforride, 
                                     Fraction_Total_Understand)
@@ -28,46 +44,76 @@ shinyServer(function(input, output) {
       colorVar <- as.numeric(input$color_var_understand)
       cols2 <- as.numeric(input$plotChoices_Understand)
       cols <- as.numeric(input$colChoices_understand)
-
+      
+      #Display the table based on column choices from user 
       if(length(input$colChoices_understand) == 1){
         df <- data.frame(datafile_understand_table[,cols])
         names(df) <- names(datafile_understand_table)[cols]
         output$table <- DT::renderDataTable(df, filter = "top")
+        # Allow user to download csv file of understand data that has been filtered
+        # Filters determined by user on application table output
+        output$down_filter_understand <- downloadHandler(
+          filename = "Seedling_Filtered_Understand_Data.csv",
+          content = function(file_out){
+            s = input$table_rows_all
+            write.csv(datafile_understand_table[s, ,drop = FALSE], file_out)
+          }
+        )
       }
       else{
         output$table <- DT::renderDataTable(datafile_understand_table[,cols], filter = "top")
+        # Allow user to download csv file of understand data that has been filtered
+        # Filters determined by user on application table output
+        output$down_filter_understand <- downloadHandler(
+          filename = "Seedling_Filtered_Understand_Data.csv",
+          content = function(file_out){
+            s = input$table_rows_all
+            write.csv(datafile_understand_table[s, ,drop = FALSE], file_out)
+          }
+        )
       }
       
+      # If only one input for plot choices; no plot can be created
       if(length(input$plotChoices_Understand) == 1){
         df2 <- data.frame(datafile_understand[,cols2])
         names(df2) <- names(datafile_understand)[cols2]
       }
       
+      # If selection is > 33, plot will display a scatter plot 
+      # Selection 34 is fraction_total_understand
+      # This variable is better visualized by scatter plot than bar chart
       else if(cols2[2] > 33){
+        #Create axis labels 
         x_lab <- colnames(datafile_understand)[cols2[1]]
         y_lab <- colnames(datafile_understand)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
         colorV <- colnames(datafile)[colorVar]
         
+        # Construct the plot from cols2 inputs 
         p <- ggplot(data = datafile_understand) +
           geom_point(mapping = aes(x = datafile_understand[cols2[1]],
                                    y = datafile_understand[cols2[2]],
                                    color = datafile[[colorV]]),
                      position = position_jitter(w = 0.2, h = 0.1),
                      alpha = .6) +
+          # Add trend line on top of scatter plot 
           stat_smooth(mapping = aes(x = datafile_understand[cols2[1]],
                                     y = datafile_understand[cols2[2]]))+
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
-        
+        # Add labels to scatter plot axes 
         p + labs(x = x_lab , y = y_lab, color = colorV)
       }
+      
+      # Input is not fraction_total_understand; display a bar chart
       else{
+        # Create axis labels 
         x_lab <- colnames(datafile_understand)[cols2[1]]
         y_lab <- colnames(datafile_understand)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
         colorV <- colnames(datafile)[colorVar]
         
+        # Construct the bar chart from cols2 inputs
         p <- ggplot(data = datafile_understand) +
           geom_bar(mapping = aes(x = datafile_understand[cols2[1]],
                                  y = datafile_understand[cols2[2]],
@@ -75,10 +121,14 @@ shinyServer(function(input, output) {
                    stat = "identity")+
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
+        # Add axes labels to chart
         p + labs(x = x_lab, y = y_lab, color = colorV)
       }
     }
+    
+    # If user selects "Talk" in radioButton input location
     else if(x() == 2){
+      # Create new dataframes for plot and table with columns representing "talk" data only
       datafile_talk <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
                               Talk_baabaa:Talk_some, Fraction_Total_Talk)
       datafile_talk_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
@@ -88,45 +138,76 @@ shinyServer(function(input, output) {
       cols2 <- as.numeric(input$plotChoices_Talk)
       cols <- as.numeric(input$colChoices_talk)
       
+      # Display the table based on column choices from user
       if(length(input$colChoices_talk) == 1){
         df <- data.frame(datafile_talk_table[,cols])
         names(df) <- names(datafile_talk_table)[cols]
         output$table = DT::renderDataTable(df, filter = "top")
+        # Allow user to download csv file of understand data that has been filtered
+        # Filters determined by user on application table output
+        output$down_filter_talk <- downloadHandler(
+          filename = "Seedling_Filtered_Talk_Data.csv",
+          content = function(file_out){
+            s = input$table_rows_all
+            write.csv(datafile_talk_table[s, ,drop = FALSE], file_out)
+          }
+        )
       }
       else{
         output$table = DT::renderDataTable(datafile_talk_table[,cols], filter = "top")
+        # Allow user to download csv file of understand data that has been filtered
+        # Filters determined by user on application table output
+        output$down_filter_talk <- downloadHandler(
+          filename = "Seedling_Filtered_Talk_Data.csv",
+          content = function(file_out){
+            s = input$table_rows_all
+            write.csv(datafile_talk_table[s, ,drop = FALSE], file_out)
+          }
+        )
       }
       
+      # If only one input for plot choices; no plot can be created
       if(length(input$plotChoices_Talk) == 1){
         df2 <- data.frame(datafile_talk[,cols2])
         names(df2) <- names(datafile_talk)[cols2]
       }
       
+      # If selection is > 398, plot will display a scatter plot 
+      # Selection 34 is fraction_total_talk
+      # This variable is better visualized by scatter plot than bar chart
       else if(cols2[2] > 398){
+        #Create axis labels
         x_lab <- colnames(datafile_talk)[cols2[1]]
         y_lab <- colnames(datafile_talk)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
         colorV <- colnames(datafile)[colorVar]
         
+        # Construct the plot from cols2 inputs 
         p <- ggplot(data = datafile_talk) +
           geom_point(mapping = aes(x = datafile_talk[cols2[1]],
                                    y = datafile_talk[cols2[2]],
                                    color = datafile[[colorV]]),
                      position = position_jitter(w = 0.2, h = 0.1),
                      alpha = .6) +
+          # Add trend line to scatter plot
           stat_smooth(mapping = aes(x = datafile_talk[cols2[1]],
                                     y = datafile_talk[cols2[2]]))+
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
         
+        # Add axis labels to scatter plot
         p + labs(x = x_lab , y = y_lab, color = colorV)
       }
+      
+      # Input is not fraction_total_talk; display a bar chart
       else{
+        # Create axis labels
         x_lab <- colnames(datafile_talk)[cols2[1]]
         y_lab <- colnames(datafile_talk)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
         colorV <- colnames(datafile)[colorVar]
         
+        # Construct the bar chart from cols2 inputs
         p <- ggplot(data = datafile_talk) +
           geom_bar(mapping = aes(x = datafile_talk[cols2[1]],
                                  y = datafile_talk[cols2[2]],
@@ -134,10 +215,14 @@ shinyServer(function(input, output) {
                    stat = "identity")+
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
+        # Add axis labels to bar chart
         p + labs(x = x_lab, y = y_lab, color = colorV)
       }
     }
+    
+    # If user selects "Gestures" in radioButton input location
     else if(x() == 3){
+      # Create new dataframes for plot and table with columns representing "gestures" data only
       datafile_gesture <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
                                  Gestures_peekaboo:Gestures_wearglasses, Fraction_Total_Gesture)
       datafile_gesture_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
@@ -145,47 +230,80 @@ shinyServer(function(input, output) {
                                        Fraction_Total_Gesture)
 
       cols <- as.numeric(input$colChoices_gesture)
+      
+      # Display the table based on column choices from user
       if(length(input$colChoices_gesture) == 1){
         df <- data.frame(datafile_gesture_table[,cols])
         names(df) <- names(datafile_gesture_table)[cols]
         output$table = DT::renderDataTable(df, filter = "top")
+        # Allow user to download csv file of understand data that has been filtered
+        # Filters determined by user on application table output
+        output$down_filter_gesture <- downloadHandler(
+          filename = "Seedling_Filtered_Gestures_Data.csv",
+          content = function(file_out){
+            s = input$table_rows_all
+            write.csv(datafile_gesture_table[s, ,drop = FALSE], file_out)
+          }
+        )
       }
       else{
         output$table = DT::renderDataTable(datafile_gesture_table[,cols], filter = "top")
+        # Allow user to download csv file of understand data that has been filtered
+        # Filters determined by user on application table output
+        output$down_filter_gesture <- downloadHandler(
+          filename = "Seedling_Filtered_Gestures_Data.csv",
+          content = function(file_out){
+            s = input$table_rows_all
+            write.csv(datafile_gesture_table[s, ,drop = FALSE], file_out)
+          }
+        )
       }
 
       colorVar <- as.numeric(input$color_var_gestures)
       cols2 <- as.numeric(input$plotChoices)
+      
+      # If only one input for plot choices; no plot can be created
       if(length(input$plotChoices) == 1){
         df2 <- data.frame(datafile_gesture[,cols2])
         names(df2) <- names(datafile_gesture)[cols2]
       }
+      
+      # If selection is > 53, plot will display a scatter plot 
+      # Selection 34 is fraction_total_gestures
+      # This variable is better visualized by scatter plot than bar chart
       else if(cols2[2] > 53){
+        #Create axis labels
         x_lab <- colnames(datafile_gesture)[cols2[1]]
         y_lab <- colnames(datafile_gesture)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
         colorV <- colnames(datafile)[colorVar]
 
-        
+        # Construct the plot from cols2 inputs 
         p <- ggplot(data = datafile_gesture) +
           geom_point(mapping = aes(x = datafile_gesture[cols2[1]],
                                    y = datafile_gesture[cols2[2]],
                                    color = datafile[[colorV]]),
                      position = position_jitter(w = 0.2, h = 0.1),
                      alpha = .6) +
+          # Add trend line to scatter plot
           stat_smooth(mapping = aes(x = datafile_gesture[cols2[1]],
                                     y = datafile_gesture[cols2[2]]))+
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
-
+        
+        # Add axis labels to scatter plot
         p + labs(x = x_lab , y = y_lab, color = colorV)
       }
+      
+      # Input is not fraction_total_understand; display a bar chart
       else{
+        # Create axis labels
         x_lab <- colnames(datafile_gesture)[cols2[1]]
         y_lab <- colnames(datafile_gesture)[cols2[2]]
         title <- paste(x_lab, "vs.", y_lab, sep = " ")
         colorV <- colnames(datafile)[colorVar]
 
+        # Construct the bar chart from cols2 inputs
         p <- ggplot(data = datafile_gesture) +
           geom_bar(mapping = aes(x = datafile_gesture[cols2[1]],
                                  y = datafile_gesture[cols2[2]],
@@ -193,6 +311,7 @@ shinyServer(function(input, output) {
                    stat = "identity") +
           ggtitle(title)+
           theme(plot.title = element_text(hjust = 0.5))
+        # Add labels to bar chart axes
         p + labs(x = x_lab, y = y_lab, color = colorV)
       }
     }
--- ui.R
+++ ui.R
@@ -3,58 +3,85 @@ library(DT)
 library(tidyverse)
 library(ggplot2)
 
-
+# Read in datafile to work with as datafile1
 datafile1 <- read.csv("final_cdi_merged_cleaned.csv")
 
+# Convert 'Yes' values in the csv file to = 1
+# Convert 'No' values in the csv file to = 0
+# Allows manipulation/computation of additional columns based on responses
+# Fraction_total columns need integer numbers to calculate
 for(n in names(datafile1[,c(17:19, 458:508)])){
   datafile1[[n]] <- ifelse(datafile1[[n]] == 'Yes',1,0)
 }
 
+# Mutate datafile to calculate fraction of all the words udnerstood; spoken by;
+# and gestures made by each subject in each month; add's columns to end of data
 datafile <- mutate(datafile1,
                    Fraction_Total_Understand = rowSums(datafile1[,c(17:47, 50:445)] == 1)/425,
                    Fraction_Total_Talk = rowSums(datafile1[,50:445] > 1)/395,
                    Fraction_Total_Gesture = rowSums(datafile1[,458:508])/51)
 
+
+# Create vector of names to be used as choices for the color scheme of the plot output
 color_choices <- 1:ncol(datafile)
 names(color_choices) <- names(datafile)
 
 
+# Create subset of datafile for table containing only "understand" information
 datafile_understand_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
                                     Child_gender, Understand_childname:Understand_goforride,
                                     Fraction_Total_Understand)
 Vchoices_table_understand <- 1:ncol(datafile_understand_table)
+# Use names from the datafile as a vector stripping the preceding "Understand_" strings
+# Therefore displaying only the word to  the user
 names(Vchoices_table_understand) <- gsub("Understand_","",names(datafile_understand_table))
 
 
+# Create subset of datafile for table containing only "talk" information
 datafile_talk_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
                               Child_gender, Talk_baabaa:Talk_some, Fraction_Total_Talk)
 Vchoices_table_talk <- 1:ncol(datafile_talk_table)
+# Use names from the datafile as a vector stripping the preceding "Talk_" strings
+# Therefore displaying only the word to  the user
 names(Vchoices_table_talk) <- gsub("Talk_","",names(datafile_talk_table))
 
 
+# Create subset of datafile for table containing only "gestures" information
 datafile_gesture_table <- select(datafile, SubjectNumber_Month:AgeMonthCDI_Corrected,
                                 Child_gender, Gestures_peekaboo:Gestures_wearglasses, Fraction_Total_Gesture)
 Vchoices_table_gesture <- 1:ncol(datafile_gesture_table)
+# Use names from the datafile as a vector stripping the preceding "Gestures_" strings
+# Therefore displaying only the word to  the user
 names(Vchoices_table_gesture) <- gsub("Gestures_","",names(datafile_gesture_table))
 
 
+# Create subset of datafile for plot output containing only "understand" information
 datafile_understand <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI, 
                               Understand_childname:Understand_goforride, 
                               Fraction_Total_Understand)
 Vchoices_understand <- 1:ncol(datafile_understand)
+# Use names from the datafile as a vector stripping the preceding "Understand_" strings
+# Therefore displaying only the word to  the user
 names(Vchoices_understand) <- gsub("Understand_","",names(datafile_understand))
 
 
+# Create subset of datafile for plot output containing only "talk" information
 datafile_talk <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
                         Talk_baabaa:Talk_some, Fraction_Total_Talk)
 Vchoices_talk <- 1:ncol(datafile_talk)
+# Use names from the datafile as a vector stripping the preceding "Talk_" strings
+# Therefore displaying only the word to  the user
 names(Vchoices_talk) <- gsub("Talk_","",names(datafile_talk))
 
 
+# Create subset of datafile for plot output containing only "gestures" information
 datafile_gesture <- select(datafile, AgeMonthCDI_Uncorrected, AgeDaysCDI,
                            Gestures_peekaboo:Gestures_wearglasses, Fraction_Total_Gesture)
 Vchoices_gesture <- 1:ncol(datafile_gesture)
+# Use names from the datafile as a vector stripping the preceding "Gestures_" strings
+# Therefore displaying only the word to  the user
 names(Vchoices_gesture) <- gsub("Gestures_","",names(datafile_gesture))
+
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -62,81 +89,127 @@ shinyUI(fluidPage(
   # Application title
   titlePanel(h1("Seedling Survey Results")),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a widgets for user manipulation of data and display in main panel
+  # 1st Download Button and Radio Button remain present regardless of user input
   sidebarLayout(
     sidebarPanel(
+      # Help text describes what the following widget does and what the user should do
+      helpText("Download data to a csv file containing 'understand,' 'talk,' and 'gestures,' information:"),
+      # Create button to download csv file from the app to user's local machine.
+      downloadButton("downloadData", "Download Full Data"),
       helpText("Select word/phrase options to analyze:"),
+      # Allows user to select 1 of the choices; this input impacts the possible inputs for table and plot
       radioButtons("radioButton", label = "Select set of words/phrases: ",
                          choices = list("Understand" = 1, "Talk" = 2, "Gestures" = 3)),
+      
       br(),
+      
+      # Conditional Panels only appear if the condition is met
+      # These panels based onuser input in the radioButton widget above
       conditionalPanel(
+        # If user selects "Understand" in radioButton, these widgets are displayed
         condition = "input.radioButton == 1",
         helpText("Select the columns that you wish to view in the data table
                  (0 = child does not understand the word, 1 = child understands the word)."),
+        # Cretes dropdown menu of choices for user to select to be displayed in the table
+        # Choices from "datafile_understand_table" only 
         selectInput("colChoices_understand", label = "Select Columns for Data Table: ",
                     choices = Vchoices_table_understand,
-                    multiple = TRUE)),
+                    multiple = TRUE),
+        # User can download subset of data containing only "understand" information
+        # From datafile_understand_table
+        helpText("Download the filtered data to csv file"),
+        downloadButton("down_filter_understand", "Download Filtered Data")),
       
       conditionalPanel(
+        # If user selects "Talk" in radioButton, these widgets are displayed
         condition = "input.radioButton == 2",
         helpText("Select the columns that you wish to view in the data table 
                  (0 = child can not say the word, 1 = child can say the word)."),
+        # Cretes dropdown menu of choices for user to select to be displayed in the table
+        # Choices from "datafile_talk_table" only
         selectInput("colChoices_talk", label = "Select Columns for Data Table: ",
                     choices = Vchoices_table_talk,
-                    multiple = TRUE)),
+                    multiple = TRUE),
+        # User can download subset of data containing only "talk" information
+        # From datafile_talk_table"
+        helpText("Download the filtered data to csv file"),
+        downloadButton("down_filter_talk", "Download Filtered Data")),
 
       conditionalPanel(
+        # If user selects "Gestures" in radioButton, these widgets are displayed
         condition = "input.radioButton == 3",
         helpText("Select the columns that you wish to view in the data table 
                  (0 = child does not make the gesture, 1 = child makes the gesture)."),
+        # Cretes dropdown menu of choices for user to select to be displayed in the table
+        # Choices from "datafile_gestures_table" only
         selectInput("colChoices_gesture", label = "Select Columns for Data Table: ",
                     choices = Vchoices_table_gesture,
-                    multiple = TRUE)),
+                    multiple = TRUE),
+        # User can download subset of data containing only "gestures" information
+        # From datafile_gestures_table"
+        helpText("Download the filtered data to csv file"),
+        downloadButton("down_filter_gesture", "Download Filtered Data")),
+      
       br(),
       conditionalPanel(
+        # If user selects "Understand" in radioButton, these widgets are displayed
         condition = "input.radioButton == 1",
         helpText("Select the two variables you wish to view in the plot.
                First choice = X-axis; Second choice = Y-axis."),
+        # Cretes dropdown menu of choices for user to select to be displayed in the plot
+        # Choices from "datafile_understand" only
         selectizeInput("plotChoices_Understand", label = "Understand Word Choices: ",
                        choices = Vchoices_understand, selected = 1,
                        options = list(maxItems = 2)),
         helpText("Select the variable with respect to which the plot colors
                will be filtered."),
+        # Cretes dropdown menu of choices for user to select color to be displayed in the plot
         selectInput("color_var_understand", label = "Select Variable for Understand Plot Color Scale:",
                     choices = color_choices, selected = 4,
                     multiple = FALSE)),
       
       conditionalPanel(
+        # If user selects "Talk" in radioButton, these widgets are displayed
         condition = "input.radioButton == 2",
         helpText("Select the two variables you wish to view in the plot.
                First choice = X-axis; Second choice = Y-axis."),
+        # Cretes dropdown menu of choices for user to select to be displayed in the plot
+        # Choices from "datafile_talk" only
         selectizeInput("plotChoices_Talk", label = "Spoken Word Choices: ",
                        choices = Vchoices_talk, selected = 1,
                        options = list(maxItems = 2)),
         helpText("Select the variable with respect to which the plot colors
                will be filtered."),
+        # Cretes dropdown menu of choices for user to select color to be displayed in the plot
         selectInput("color_var_talk", label = "Select Variable for Talk Plot Color Scale:",
                     choices = color_choices, selected = 4,
                     multiple = FALSE)),
       
       conditionalPanel(
+        # If user selects "Gestures" in radioButton, these widgets are displayed
         condition = "input.radioButton == 3",
         helpText("Select the two variables you wish to view in the plot.
                 First choice = X-axis; Second choice = Y-axis."),
+        # Cretes dropdown menu of choices for user to select to be displayed in the plot
+        # Choices from "datafile_gestures" only
         selectizeInput("plotChoices", label = "Select Gestures: ",
                        choices = Vchoices_gesture, selected = 1,
                        options = list(maxItems = 2)),
         helpText("Select the variable with respect to which the plot colors
                  will be filtered."),
+        # Cretes dropdown menu of choices for user to select color to be displayed in the plot
         selectInput("color_var_gestures", label = "Select Variable for Gestures Plot Color Scale:",
                     choices = color_choices, selected = 4,
                     multiple = FALSE))
     ),
   
-    # Show a plot of the generated distribution
+    # Show a the table and plot from user input in sidebar panel widgets
     mainPanel(
       h2("Survey Results", align = "center"),
+      #Data Table output
       DT::dataTableOutput("table"),
+      #Plot output
       plotOutput("plot", height = 500)
     )
   )
